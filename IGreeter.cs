b7653861ddc30ca43d1a3ebd3dbbd4c347151b0a
tfleming <tfleming26@hotmail.com>
2018-05-02 10:52:42 -0600

    Create greeter interface

---
+++ IGreeter.cs
@@ -0,0 +1,15 @@
+﻿namespace AnOdeToGrub
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        public string GetMessageOfTheDay()
+        {
+            return "Greetings Patrick Star!";
+        }
+    }
+}
